8f975669586f7904dea55fdf7459e0307f4396e5
BsHeR4 <bshermahayni@gmail.com>
2025-06-01 12:02:32 +0300

    Add README file

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -5,11 +5,56 @@ namespace App\Services;
 use App\Repositories\PostRepository;
 use App\Services\Base\BaseService;
 use App\Services\Interfaces\PostServiceInterface;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class PostService extends BaseService implements PostServiceInterface
 {
     public function __construct(PostRepository $repository)
     {
         $this->repository = $repository;
+    }
+
+    public function getAll(array $filters = [])
+    {
+        return $this->handle(
+            function () use ($filters) {
+
+                $key = empty($filters) ? 'posts' : 'posts.' . md5(json_encode($filters));
+
+                $time = empty($filters) ? 3600 : 180;
+
+                return Cache::remember(
+                    $key,
+                    $time,
+                    function () use ($filters) {
+                        return parent::getAll($filters);
+                    }
+                );
+            }
+        );
+    }
+
+    public function store(array $data)
+    {
+        return $this->handle(function () use ($data) {
+
+            $post = parent::store($data);
+            
+            Cache::forget('posts');
+
+            return $post;
+        });
+    }
+
+    public function update(array $data, string|Model $id)
+    {
+        return $this->handle(function () use ($data, $id) {
+            $post = parent::update($data, $id);
+
+            Cache::forget('posts');
+
+            return $post;
+        });
     }
 }
